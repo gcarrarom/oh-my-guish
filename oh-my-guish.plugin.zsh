d764034f32ddc5a2681936471e3e33090395e80c
Gui Martins <gui.martins.94@outlook.com>
2020-09-08 09:00:07 -0300

    Added _encode function

--- oh-my-guish.plugin.zsh
+++ oh-my-guish.plugin.zsh
@@ -103,6 +103,18 @@ function kreport() {
     RAM avg(MB): $memnum = $mempercent%"
 }
 
+_encode() {
+    local _length="${#1}"
+    for (( _offset = 0 ; _offset < _length ; _offset++ )); do
+        _print_offset="${1:_offset:1}"
+        case "${_print_offset}" in
+            [a-zA-Z0-9.~_-]) printf "${_print_offset}" ;;
+            '/') printf '/' ;;
+            ':') printf ':' ;;
+            *) printf '%%%X' "'${_print_offset}" ;;
+        esac
+    done
+}
 function holog() {
     since=$1
     if [[ -z "$since" ]]; then
